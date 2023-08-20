4bbe15bf647c89decfbc04eec69b34202b0b5db7
fangd123 <fangd123@qq.com>
2023-08-20 21:12:23 +0800

    添加说话人识别分组接口

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ from pydub import AudioSegment
 inference_pipeline = pipeline('auto-speech-recognition',
                               'damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
 
+sd_pipeline = pipeline(
+    task='speaker-diarization',
+    model='damo/speech_campplus_speaker-diarization_common',
+    model_revision='v1.0.0'
+)
+
 app = FastAPI()
 origins = [
     "http://localhost.tiangolo.com",
@@ -54,6 +60,45 @@ def convert_to_wav(input_file, output_file):
     audio.export(output_file, format="wav")
 
 
+def merge_single_word_segments(output_data):
+    merged_data = []
+
+    i = 0
+    while i < len(output_data):
+        segment = output_data[i]
+
+        # Check if it's a single-word segment and the next segment has the same speaker
+        if len(segment["text"].strip()) == 1:
+            merged_segment = {
+                "text": segment["text"],
+                "start": segment["start"],
+                "end": segment["end"],
+                "speaker": segment["speaker"]
+            }
+
+            # Try to merge with subsequent segments
+            while i + 1 < len(output_data) and len(output_data[i + 1]["text"].split()) == 1 and output_data[i + 1][
+                "speaker"] == merged_segment["speaker"]:
+                merged_segment["text"] += " " + output_data[i + 1]["text"]
+                merged_segment["end"] = output_data[i + 1]["end"]
+                i += 1
+
+            merged_data.append(merged_segment)
+        else:
+            merged_data.append(segment)
+
+        i += 1
+
+    return merged_data
+
+
+def find_speaker_for_ts(ts, speaker_data):
+    for segment in speaker_data["text"]:
+        if segment[0] <= ts <= segment[1]:
+            return segment[2]
+    return None
+
+
 @app.post("/transcribe/")
 async def transcribe_audio_file(file: UploadFile):
     # Save the uploaded audio file
@@ -73,6 +118,7 @@ async def transcribe_audio_file(file: UploadFile):
 
     # Call the ASR model to transcribe the audio
     result = inference_pipeline(waveform)
+    sd_result = sd_pipeline(waveform)
 
     # Clean up the temporary audio file
     os.remove(audio_path)
@@ -80,7 +126,90 @@ async def transcribe_audio_file(file: UploadFile):
     if len(result['sentences']) == 1:
         return [
             {"text": result["text"], 'start': result['sentences'][0]["start"], 'end': result['sentences'][0]["end"]}]
-    return result['sentences']
+
+    asr_data = result['sentences']
+
+    for speaker_segment in sd_result["text"]:
+        speaker_segment[0] = speaker_segment[0] * 1000
+        speaker_segment[1] = speaker_segment[1] * 1000
+        speaker_segment[2] = int(speaker_segment[2])
+
+    output_data = []
+
+    for segment in asr_data:
+        previous_speaker = None
+        text_list = segment['text_seg'].split()
+        modified = False  # Track if the segment is split
+
+        for idx, ts_segment in enumerate(segment['ts_list']):
+            current_speaker = find_speaker_for_ts(ts_segment[0], sd_result)
+
+            if previous_speaker is None:
+                previous_speaker = current_speaker
+
+            # If speaker changes in the middle of a segment
+            if current_speaker != previous_speaker:
+                modified = True
+                new_seg = {
+                    "text": ' '.join(text_list[:idx]),
+                    "start": segment['start'],
+                    "end": ts_segment[0],
+                    "speaker": previous_speaker
+                }
+                output_data.append(new_seg)
+
+                segment['start'] = ts_segment[0]
+                text_list = text_list[idx:]
+
+                previous_speaker = current_speaker
+
+        # Append the last or only segment
+        if modified:
+            new_seg = {
+                "text": ' '.join(text_list),
+                "start": segment['start'],
+                "end": segment['end'],
+                "speaker": previous_speaker
+            }
+        else:
+            # If not modified, use the original text value
+            new_seg = {
+                "text": segment['text'],
+                "start": segment['start'],
+                "end": segment['end'],
+                "speaker": previous_speaker
+            }
+        output_data.append(new_seg)
+
+    output_data = merge_single_word_segments(output_data)
+
+    # Group by speaker while preserving the time order
+    final_output = []
+
+    current_speaker_data = None
+    for item in output_data:
+        if current_speaker_data and current_speaker_data["speaker"] == item["speaker"]:
+            current_speaker_data["sentences"].append({
+                "text": item["text"],
+                "start": item["start"],
+                "end": item["end"],
+                "speaker": item["speaker"]
+            })
+        else:
+            if current_speaker_data:
+                final_output.append(current_speaker_data)
+            current_speaker_data = {"speaker": item["speaker"], "sentences": [{
+                "text": item["text"],
+                "start": item["start"],
+                "end": item["end"],
+                "speaker": item["speaker"]
+            }]}
+
+    if current_speaker_data:
+        final_output.append(current_speaker_data)
+
+    print(final_output)
+    return final_output
 
 
 if __name__ == '__main__':
